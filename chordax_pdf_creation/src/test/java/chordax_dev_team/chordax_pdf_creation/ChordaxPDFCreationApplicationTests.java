ccaf058e9d7fee57079dfd5183148e72e1c70f39
piotr.bar <piotr.bar@accenture.com>
2025-09-20 15:51:22 +0100

    Set config-server in pom.xmp and application.yml files

--- chordax_pdf_creation/src/test/java/chordax_dev_team/chordax_pdf_creation/ChordaxPDFCreationApplicationTests.java
+++ chordax_pdf_creation/src/test/java/chordax_dev_team/chordax_pdf_creation/ChordaxPDFCreationApplicationTests.java
@@ -1,13 +1,13 @@
-package chordax_dev_team.chordax_pdf_creation;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ChordaxPDFCreationApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package chordax_dev_team.chordax_pdf_creation;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ChordaxPDFCreationApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
